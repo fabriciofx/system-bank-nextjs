08bda4473d9ab246ce7a7bb23902e2718b7a2a25
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-16 14:31:50 -0300

    feat(cliente): adiciona página para cadastrar um novo cliente

---
+++ app/clientes/novo/page.tsx
@@ -0,0 +1,27 @@
+import FormCliente from '../../../src/components/cliente/FormCliente';
+import Navbar from '../../../src/components/navbar/Navbar';
+import {
+  clienteById,
+  createCliente,
+  updateCliente
+} from '../../../src/services/ClienteService';
+import styles from './novo.module.css';
+
+export default function NovoClientePage() {
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.box_com_titulo}>
+        <div className={styles.box_cliente}>
+          <h1>Novo cliente</h1>
+          <FormCliente
+            create={createCliente}
+            update={updateCliente}
+            findById={clienteById}
+            buttonText="Cadastrar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/cliente/FormCliente.tsx
@@ -0,0 +1,131 @@
+'use client';
+
+import { Button, FormControlLabel, Switch, TextField } from '@mui/material';
+import { useParams, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { ErrorMessage, SuccessMessage } from '../../components/message/Message';
+import { CLIENTE_INVALIDO, type Cliente } from '../../models/Cliente';
+import styles from './FormCliente.module.css';
+
+type FormClienteProps = {
+  create: (cliente: Cliente) => Promise<Cliente>;
+  update: (id: number, clienteAtualizado: Cliente) => Promise<Cliente>;
+  findById: (id: number) => Promise<Cliente[]>;
+  buttonText: string;
+};
+
+export default function FormCliente({
+  create,
+  update,
+  findById,
+  buttonText
+}: FormClienteProps) {
+  const router = useRouter();
+  const { id } = useParams();
+  const [cliente, setCliente] = useState<Cliente>(CLIENTE_INVALIDO);
+
+  useEffect(() => {
+    if (id) {
+      (async () => {
+        const clienteEdit = await findById(Number(id));
+        if (clienteEdit.length > 0) {
+          setCliente(clienteEdit[0]);
+        }
+      })();
+    }
+  }, [findById, id]);
+
+  function handleChange(
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void {
+    const { name, value } = event.target;
+    setCliente({ ...cliente, [name]: value });
+  }
+
+  async function handleSubmit(
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+    try {
+      if (cliente.id) {
+        await update(cliente.id, cliente);
+        await new SuccessMessage(
+          'Sucesso!',
+          'Cliente atualizado com sucesso!'
+        ).show();
+      } else {
+        const novoCliente = await create(cliente);
+        if (novoCliente.id === 0) {
+          throw new Error('o identificador do cliente não retornou');
+        }
+        await new SuccessMessage(
+          'Sucesso!',
+          'Cliente cadastrado com sucesso!'
+        ).show();
+      }
+      router.push('/clientes');
+    } catch (error) {
+      await new ErrorMessage(
+        'Oops...',
+        `Erro ao cadastrar/atualizar o cliente: ${error}`
+      ).show();
+    }
+  }
+
+  return (
+    <div>
+      <form className={styles.form_cliente} onSubmit={handleSubmit}>
+        <TextField
+          label="Nome"
+          name="nome"
+          required
+          value={cliente.nome}
+          onChange={handleChange}
+          variant="filled"
+        />
+        <TextField
+          label="CPF"
+          name="cpf"
+          required
+          value={cliente.cpf}
+          onChange={handleChange}
+          variant="filled"
+        />
+        <TextField
+          label="E-mail"
+          type="email"
+          name="email"
+          required
+          value={cliente.email}
+          onChange={handleChange}
+          variant="filled"
+        />
+        <TextField
+          label="Observações"
+          name="observacoes"
+          multiline
+          minRows={3}
+          maxRows={6}
+          required
+          value={cliente.observacoes}
+          onChange={handleChange}
+          variant="filled"
+        />
+        <FormControlLabel
+          control={
+            <Switch
+              name="ativo"
+              checked={cliente.ativo}
+              required
+              onChange={() => setCliente({ ...cliente, ativo: !cliente.ativo })}
+            />
+          }
+          label={cliente.ativo ? 'Ativo' : 'Inativo'}
+        />
+        <Button type="submit" variant="contained">
+          {buttonText}
+        </Button>
+      </form>
+    </div>
+  );
+}
