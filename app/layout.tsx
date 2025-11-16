b7ee602f929fc2ffd63b507417180cf9e25d05cc
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-16 10:53:49 -0300

    refactor: move o query client provider para o layout principal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
-import type { Metadata } from 'next';
-import MuiThemeProvider from '@/src/theme/MuiThemeProvider';
+'use client';
 
-export const metadata: Metadata = {
-  title: 'SystemBank',
-  description: 'Aplicação de exemplo de utilização do Next.JS'
-};
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import MuiThemeProvider from '@/src/theme/MuiThemeProvider';
 
 export default function AppLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient();
   return (
     <html lang="pt-BR">
       <body>
-        <MuiThemeProvider>{children}</MuiThemeProvider>
+        <MuiThemeProvider>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </MuiThemeProvider>
       </body>
     </html>
   );
