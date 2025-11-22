fbc72c4f845adc84a73d2ed0cc6ada35fbee2098
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-22 18:09:51 -0300

    style: troca css por tailwindcss no componente spinner

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import styles from './Spinner.module.css';
-
 export default function Spinner() {
   return (
-    <div className={styles.spinner_overlay}>
-      <div className={styles.spinner} />
+    <div className="fixed top-0 left-0 w-full h-full bg-white/60 flex justify-center items-center z-[9999]">
+      <div className="w-20 h-20 border-4 border-solid border-[#ddd] border-t-[#007bff] rounded-full animate-spin" />
     </div>
   );
 }
