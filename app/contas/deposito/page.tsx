bba75581971b0ba4aa8fa25bcce555c304934fcb
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-22 17:36:29 -0300

    style: troca css por tailwindcss na página de depósito

--- app/contas/deposito/page.tsx
+++ app/contas/deposito/page.tsx
@@ -3,15 +3,14 @@
 import { useDeposito } from '@/src/hooks/useDeposito';
 import DepositoForm from '../../../src/components/conta/DepositoForm';
 import Navbar from '../../../src/components/navbar/Navbar';
-import styles from './deposito.module.css';
 
 export default function DepositoPage() {
   return (
     <div>
       <Navbar />
-      <div className={styles.box_com_titulo}>
-        <div className={styles.box_conta}>
-          <h1>Depósito</h1>
+      <div className="flex flex-col justify-start items-center">
+        <div className="flex flex-col justify-start items-start">
+          <h1 className="text-4xl font-extrabold mb-4">Depósito</h1>
           <DepositoForm deposita={useDeposito} />
         </div>
       </div>
