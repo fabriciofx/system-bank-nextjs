7245fa8c8b73a8c2f5e13728f53bb6da5c129fd9
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-22 09:49:47 -0300

    feat: habilita o tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import MuiThemeProvider from '@/src/theme/MuiThemeProvider';
+import './globals.css';
 
 export default function AppLayout({
   children
