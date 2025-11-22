12177470ab9a287b467d44832278f1665eec15db
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-22 17:42:19 -0300

    style: troca css por tailwindcss na página de listar contas

--- app/contas/page.tsx
+++ app/contas/page.tsx
@@ -6,14 +6,13 @@ import { useDeleteConta } from '@/src/hooks/useConta';
 import ContasList from '../../src/components/conta/ContasList';
 import Navbar from '../../src/components/navbar/Navbar';
 import { usePagesContasClientes } from '../../src/hooks/useContasClientes';
-import styles from './contas.module.css';
 
 export default function ContasPage() {
   return (
     <div>
       <Navbar />
-      <h1>Listagem de contas</h1>
-      <div className={styles.buttons_conta}>
+      <h1 className="text-4xl font-extrabold mb-4">Listagem de contas</h1>
+      <div className="flex justify-start items-start gap-4 mb-4">
         <Button
           variant="contained"
           color="primary"
