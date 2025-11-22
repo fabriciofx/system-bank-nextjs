bf01514499eb422af8def614a6aff9a25c0507f8
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-22 13:30:46 -0300

    fix: corrige o teste de login form

--- src/components/login/LoginForm.test.tsx
+++ src/components/login/LoginForm.test.tsx
@@ -1,4 +1,4 @@
-import { MemoryRouter, type NavigateFunction } from 'react-router-dom';
+import type { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { describe, expect, it, vi } from 'vitest';
 import { render } from 'vitest-browser-react';
 import { fakeLogin } from '../../services/FakeAuthService';
@@ -6,17 +6,22 @@ import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
   it('deve autenticar o usuário', async () => {
-    const fakeNavigate = vi.fn() as unknown as NavigateFunction;
+    const fakeRouter = {
+      push: vi.fn(),
+      replace: vi.fn(),
+      prefetch: vi.fn(),
+      refresh: vi.fn(),
+      back: vi.fn(),
+      forward: vi.fn()
+    } as AppRouterInstance;
     const screen = await render(
-      <MemoryRouter>
-        <LoginForm login={fakeLogin} router={fakeNavigate} />
-      </MemoryRouter>
+      <LoginForm login={fakeLogin} router={fakeRouter} />
     );
     await vi.waitFor(async () => {
       await screen.getByLabelText(/e-mail ou nome de usuário/i).fill('teste');
       await screen.getByLabelText(/senha/i).fill('12345678');
       await screen.getByRole('button').click();
-      expect(fakeNavigate).toHaveBeenCalledWith('/clientes');
+      expect(fakeRouter.push).toHaveBeenCalledWith('/clientes');
     });
   });
 });
