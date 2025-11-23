933118f71658821e13f12650325a45e2e07fffdb
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-23 15:14:39 -0300

    style: simplifica o tailwindcss do form login

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -32,10 +32,10 @@ export default function LoginForm({ login, router }: LoginFormProps) {
   }
 
   return (
-    <div className="flex flex-col mt-[5%] mx-[15%] mb-[15%]">
-      <div className="mb-[20%]">
-        <h1 className="text-[28px] font-bold mb-3">Login</h1>
-        <p className="font-normal text-base text-[#737e88] mt-0">
+    <div className="flex flex-col mb-8">
+      <div className="mb-16">
+        <h1 className="text-4xl font-bold">Login</h1>
+        <p className="text-gray-500">
           Faça login na área administrativa do sistema. Solicite um acesso à
           equipe técnica.
         </p>
@@ -43,16 +43,13 @@ export default function LoginForm({ login, router }: LoginFormProps) {
       <div>
         <form onSubmit={handleSubmit}>
           <div className="flex flex-col mb-10">
-            <label
-              htmlFor="username"
-              className="font-semibold text-sm text-[#0b0c0d]"
-            >
+            <label htmlFor="username" className="font-semibold text-sm">
               E-mail ou Nome de Usuário
             </label>
             <TextField
               id="username"
               name="username"
-              className="bg-white border border-solid p-4 rounded text-[18px] shadow-none"
+              className="bg-white border"
               value={credentials.username}
               onChange={handleChange}
               variant="outlined"
@@ -60,26 +57,28 @@ export default function LoginForm({ login, router }: LoginFormProps) {
             />
           </div>
           <div className="flex flex-col mb-10">
-            <label
-              htmlFor="password"
-              className="font-semibold text-sm text-[#0b0c0d]"
-            >
+            <label htmlFor="password" className="font-semibold text-sm">
               Senha
             </label>
             <TextField
               type="password"
               id="password"
               name="password"
-              className="bg-white border border-solid p-4 rounded text-[18px] shadow-none"
+              className="bg-white border"
               value={credentials.password}
               onChange={handleChange}
               variant="outlined"
               required
             />
           </div>
-          <p className="font-semibold text-sm text-[#1a77f2] underline mt-3 mb-[52px]">
-            Esqueci minha senha
-          </p>
+          <div className="mb-8">
+            <a
+              href="http://localhost:3000/recuperar-senha"
+              className="text-blue-500 underline"
+            >
+              Esqueci minha senha
+            </a>
+          </div>
           <div className="flex justify-center">
             <Button type="submit" variant="contained">
               Entrar
